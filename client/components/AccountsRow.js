77988b4449cdcfe18e47a0725560349e1d86d507
Shoshana Rosenfield <Shoshana@MacBook-Air-2.local>
2018-12-03 10:36:09 -0500

    Update per interview

--- client/components/AccountsRow.js
+++ client/components/AccountsRow.js
@@ -1,18 +1,59 @@
-import React from 'react'
+import React, {Component} from 'react'
 import { mapAccountIdsToType, accountTableData, formatNumber } from '../utilityFunctions';
 
-const AccountRow = (props) => {
-  const {total, accountsData} = props
-  const accountTypesObj = mapAccountIdsToType(accountsData)
-  const data = accountTableData(accountTypesObj)
-  data.sort((row1,row2) => row2.sum - row1.sum)
+
+class AccountRow extends Component {
+  constructor(props){
+    super(props)
+    const {total, accountsData} = props
+    const accountTypesObj = mapAccountIdsToType(accountsData)
+    const data = accountTableData(accountTypesObj)
+    this.state = {
+      data,
+      total,
+      type: true
+    }
+  }
+
+  render(){
+  const {data, total} = this.state
+  console.log('state',this.state)
   return (
     <table>
       <tbody>
         <tr className='accountsRow'>
-          <th>Type</th>
-          <th>Sum</th>
-          <th>Percent of Total</th>
+          <th onClick={()=>{
+            if(this.state.type){
+              this.setState({
+                type: false})
+              }
+              else this.setState({type:true})
+            this.setState({ data: data.sort((row1,row2) => {
+              let coerce = {
+                true: 1,
+                false: -1
+              }
+              if(this.state.type){
+                return (coerce[row2.type[0] < row1.type[0]])
+              }
+              else{
+                return -1
+              }
+
+            })
+          })}}>Type</th>
+          <th onClick={()=>{
+            this.setState({
+              data: data.sort((row1,row2) => {
+              return row2.sum - row1.sum})})
+          }}>Sum</th>
+          <th onClick={ ()=>{
+            this.setState({
+              data:data.sort((row1,row2) => {
+                return row2.sum - row1.sum})
+            })
+          }
+          }>Percent of Total</th>
         </tr>
 
         {/* map over the data array generated from the utility function accountTableData */}
@@ -30,6 +71,7 @@ const AccountRow = (props) => {
       </tbody>
     </table>
   )
+ }
 }
 
 export default AccountRow
